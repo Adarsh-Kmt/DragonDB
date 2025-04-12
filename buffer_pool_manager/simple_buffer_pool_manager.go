723be0931b82e524e269a91762b31c3f0e8896b0
Adarsh-Kmt <adarshkmt2004@gmail.com>
2025-04-12 20:28:01 +0530

    Used disk manager, LRU replacer to build a buffer pool manager.

--- buffer_pool_manager/simple_buffer_pool_manager.go
+++ buffer_pool_manager/simple_buffer_pool_manager.go
@@ -1,6 +1,6 @@
 package buffer_pool_manager
 
-import "encoding/binary"
+import "sync"
 
 const (
 	PAGE_SIZE        = 4096
@@ -9,172 +9,262 @@ const (
 	CLEAN            = false
 )
 
+type FrameID int
+type PageID uint64
+
 type BufferPoolManager interface {
-	ReadPage(pageId uint64) ([]byte, error)
-	WritePage(pageId uint64, data []byte) error
+	NewPage() PageID
 
-	AllocatePage() uint64
-	DeallocatePage(pageId uint64)
+	flushPage(pageID PageID) (bool, error)
+	flushAllPages() error
 
-	UnpinPage(pageId uint64)
-	PinPage(pageId uint64)
+	fetchPage(pageID PageID) (*Frame, error)
+
+	deletePage(pageID PageID) (bool, error)
+	unpinPage(pageID PageID) bool
+
+	Close() error
 }
 
+type Frame struct {
+	pageId PageID
+	data   []byte
+
+	pinCount int
+	dirty    bool
+
+	version int
+	mutex   *sync.RWMutex
+}
 type SimpleBufferPoolManager struct {
-	cache Cache
-	disk  *DiskManager
+	replacer Replacer
+	disk     *DiskManager
 
-	releasedPageIds    []uint64
-	maxAllocatedPageId uint64
+	pageMutex *sync.RWMutex
+	pageTable map[PageID]FrameID
+	frameList []Frame
+
+	frameAllocationMutex *sync.Mutex
+	freeFrameList        []FrameID
+	size                 int
 }
 
-func NewSimpleBufferPoolManager(cache Cache, disk *DiskManager) *SimpleBufferPoolManager {
+func NewSimpleBufferPoolManager(size int, replacer Replacer, disk *DiskManager) *SimpleBufferPoolManager {
 
-	freelistPageData, err := disk.Read(FREELIST_PAGE_ID*PAGE_SIZE, PAGE_SIZE)
-	if err != nil {
-		panic(err)
+	frameList := make([]Frame, 0)
+
+	for i := range frameList {
+		frameList[i] = Frame{}
 	}
 
-	maxAllocatedPageId, releasedPageIds := deserializeFreelistPage(freelistPageData)
+	freeFrameList := make([]FrameID, 0)
+
+	for i := 0; i < size; i++ {
+		freeFrameList = append(freeFrameList, FrameID(i))
+	}
 
 	return &SimpleBufferPoolManager{
-		cache:              cache,
-		disk:               disk,
-		releasedPageIds:    releasedPageIds,
-		maxAllocatedPageId: maxAllocatedPageId,
+		replacer: replacer,
+		disk:     disk,
+
+		pageMutex: &sync.RWMutex{},
+		pageTable: make(map[PageID]FrameID),
+		frameList: frameList,
+
+		frameAllocationMutex: &sync.Mutex{},
+		freeFrameList:        freeFrameList,
+		size:                 size,
 	}
 }
-func (pool *SimpleBufferPoolManager) ReadPage(pageId uint64, pinPage bool) ([]byte, error) {
 
-	// check if page exists in data.
-	data, exists := pool.cache.RetrievePage(pageId)
+func (bufferPool *SimpleBufferPoolManager) NewPage() PageID {
 
-	if exists {
-		if pinPage {
-			pool.cache.PinPage(pageId)
-		}
-		return data, nil
-	}
+	return bufferPool.disk.allocatePage()
+}
 
-	// read page data from disk.
-	data, err := pool.disk.Read(int64(pageId*PAGE_SIZE), PAGE_SIZE)
+func (bufferPool *SimpleBufferPoolManager) fetchPage(pageId PageID) (*Frame, error) {
 
-	if err != nil {
-		return nil, err
-	}
+	frameId, exists := bufferPool.pageTable[pageId]
 
-	// cache page that was read from disk
-	dirtyPageId, dirtyPageData := pool.cache.CachePage(pageId, data, CLEAN)
+	// --> If page table entry does not exist:
+	if !exists {
 
-	//
-	pool.cache.PinPage(pageId)
-	// if a dirty page was evicted to make room for the current page,
-	// write it to disk.
-	if dirtyPageId != nil {
-		err = pool.disk.Write(int64(*dirtyPageId), dirtyPageData)
+		// 1. Read page from file using disk manager.
+		data, err := bufferPool.disk.read(pageId*PAGE_SIZE, PAGE_SIZE)
 		if err != nil {
-			return data, err
+			return nil, err
 		}
+
+		var frameId FrameID
+
+		// 2. Check if free frame exists in free frame list.
+		if len(bufferPool.freeFrameList) > 0 {
+
+			frameId = bufferPool.freeFrameList[0]
+			bufferPool.freeFrameList = bufferPool.freeFrameList[1:]
+
+		} else {
+			// 3.0 If free frame list is empty, evict a frame using replacer.
+
+			frameId = bufferPool.replacer.victim()
+
+			// 3.1 If frame to be evicted is dirty, write to disk.
+
+			frame := bufferPool.frameList[frameId]
+
+			if frame.dirty {
+				bufferPool.disk.write(frame.pageId*PAGE_SIZE, frame.data)
+			}
+
+		}
+
+		// 4. Store page data in frame with pin count = 1
+
+		frame := bufferPool.frameList[frameId]
+
+		frame.data = data
+		frame.pageId = pageId
+		frame.pinCount = 1
+		frame.dirty = false
+
+		// 5. Update page table.
+		bufferPool.pageTable[pageId] = frameId
+
+		return &frame, nil
 	}
-	return data, nil
+
+	// --> If page table entry exists:
+
+	// 1. Fetch frameId corresponding to pageId.
+	frame := bufferPool.frameList[frameId]
+
+	// 2. If pin count of frame = 0, remove frame from LRU replacer.
+	if frame.pinCount == 0 {
+		bufferPool.replacer.remove(frameId)
+	}
+
+	// 3. Increment pin count.
+	frame.pinCount++
+
+	// 4. Return data stored in frame.
+	return &frame, nil
 
 }
 
-func (pool *SimpleBufferPoolManager) WritePage(pageId uint64, data []byte) error {
+func (bufferPool SimpleBufferPoolManager) deletePage(pageId PageID) (bool, error) {
 
-	dirtyPageId, dirtyPageData := pool.cache.CachePage(pageId, data, DIRTY)
+	// 1. Fetch frameId corresponding to pageId.
+	frameId, exists := bufferPool.pageTable[pageId]
 
-	if dirtyPageId != nil {
+	// 2. If page not in memory, return false.
+	if !exists {
+		return false, nil
+	}
 
-		err := pool.disk.Write(int64(*dirtyPageId*PAGE_SIZE), dirtyPageData)
-		if err != nil {
-			return err
+	// 3. Fetch frame.
+	frame := bufferPool.frameList[frameId]
+
+	// 4. If page is being used by others, it cannot be deleted, so return false.
+	if frame.pinCount != 1 {
+		return false, nil
+
+	}
+
+	// 5. If frame stores a dirty page, write to disk.
+	if frame.dirty {
+		if err := bufferPool.disk.write(pageId*PAGE_SIZE, frame.data); err != nil {
+			return true, err
+		}
+	}
+
+	// 6. Add frameId to freeFrameList.
+	bufferPool.freeFrameList = append(bufferPool.freeFrameList, frameId)
+
+	// 7. Delete page table entry.
+	delete(bufferPool.pageTable, pageId)
+
+	// 8. Deallocate page in file.
+	bufferPool.disk.deallocatePage(pageId)
+
+	// 9. Free up space.
+	frame.data = nil
+
+	// 10. Reset dirty, version, pageId fields of the frame
+	frame.dirty = false
+	frame.version = 1
+	frame.pageId = 0
+
+	return true, nil
+}
+
+func (bufferPool SimpleBufferPoolManager) unpinPage(pageId PageID) bool {
+
+	// 1. Fetch frameId corresponding to pageId.
+	frameId, exists := bufferPool.pageTable[pageId]
+
+	// 2. If page not in memory, return false.
+	if !exists {
+		return false
+	}
+
+	// 3. Fetch frame.
+	frame := bufferPool.frameList[frameId]
+
+	// 4. Decrement pin count.
+	frame.pinCount--
+
+	// 5. If pin count = 0, add frame to replacer.
+	if frame.pinCount == 0 {
+		bufferPool.replacer.insert(frameId)
+	}
+
+	return true
+}
+
+func (bufferPool SimpleBufferPoolManager) flushPage(pageId PageID) (bool, error) {
+
+	frameId, exists := bufferPool.pageTable[pageId]
+
+	if !exists {
+		return false, nil
+	}
+
+	frame := bufferPool.frameList[frameId]
+
+	if frame.dirty {
+		return true, bufferPool.disk.write(pageId*PAGE_SIZE, frame.data)
+	}
+
+	return true, nil
+}
+
+func (bufferPool SimpleBufferPoolManager) flushAllPages() error {
+
+	for pageId, frameId := range bufferPool.pageTable {
+
+		frame := bufferPool.frameList[frameId]
+
+		if frame.dirty {
+
+			if err := bufferPool.disk.write(pageId*PAGE_SIZE, frame.data); err != nil {
+				return err
+			}
 		}
 	}
 
 	return nil
 }
 
-func (pool *SimpleBufferPoolManager) AllocatePage() uint64 {
+func (bufferPool SimpleBufferPoolManager) Close() error {
 
-	if len(pool.releasedPageIds) == 0 {
-		pageId := pool.maxAllocatedPageId + 1
-		pool.maxAllocatedPageId += 1
-		return pageId
-	} else {
-		pageId := pool.releasedPageIds[0]
-		pool.releasedPageIds = pool.releasedPageIds[1:]
-		return pageId
-	}
-}
-
-func (pool *SimpleBufferPoolManager) DeallocatePage(pageId uint64) {
-	pool.releasedPageIds = append(pool.releasedPageIds, pageId)
-}
-
-func (pool *SimpleBufferPoolManager) Close() error {
-
-	freelistPageData := serializeFreelistPage(pool.maxAllocatedPageId, pool.releasedPageIds)
-
-	err := pool.disk.Write(int64(PAGE_SIZE*FREELIST_PAGE_ID), freelistPageData)
-
-	if err != nil {
+	if err := bufferPool.flushAllPages(); err != nil {
 		return err
 	}
 
-	dirtyPages := pool.cache.GetDirtyPages()
-
-	for pageId, data := range dirtyPages {
-
-		if err = pool.disk.Write(int64(pageId*PAGE_SIZE), data); err != nil {
-			return err
-		}
-	}
-
-	err = pool.disk.file.Close()
-
-	if err != nil {
+	if err := bufferPool.disk.Close(); err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func deserializeFreelistPage(data []byte) (maxAllocatedPageId uint64, releasedPages []uint64) {
-
-	pointer := 0
-	maxAllocatedPageId = binary.LittleEndian.Uint64(data[pointer : pointer+8])
-
-	pointer += 8
-
-	releasedPageListSize := binary.LittleEndian.Uint64(data[pointer : pointer+8])
-	pointer += 8
-
-	releasedPages = make([]uint64, 0)
-
-	for i := 0; i < int(releasedPageListSize); i++ {
-		releasedPages = append(releasedPages, binary.LittleEndian.Uint64(data[pointer:pointer+8]))
-		pointer += 8
-	}
-
-	return maxAllocatedPageId, releasedPages
-}
-
-func serializeFreelistPage(maxAllocatedPageId uint64, releasedPages []uint64) []byte {
-
-	data := make([]byte, 0)
-
-	pointer := 0
-	binary.LittleEndian.PutUint64(data[pointer:pointer+8], maxAllocatedPageId)
-	pointer += 8
-
-	binary.LittleEndian.PutUint64(data[pointer:pointer+8], uint64(len(releasedPages)))
-	pointer += 8
-
-	for _, pageId := range releasedPages {
-		binary.LittleEndian.PutUint64(data[pointer:pointer+8], pageId)
-		pointer += 8
-	}
-	return data
 
 }
